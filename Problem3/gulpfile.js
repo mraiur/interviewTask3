fcd77ac551de7760402f3ef1de51104e8bfd12cc
Nikolai Ivanov <mraiur@gmail.com>
2015-04-17 02:09:39 +0300

    Minify problem 3 Js and CSS.

---
+++ Problem3/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    bowerFiles = require('main-bower-files');
+
+gulp.task('bower', function(){
+    return gulp.src(bowerFiles())
+        .pipe( gulp.dest('public/bower/') );
+});
+
+gulp.task('clean', function(){
+    //TODO make any cleaning of directories or assets
+});
+
+gulp.task('sass', function(){
+    gulp.src('./resources/sass/*.scss')
+        .pipe( sass() )
+        .pipe( gulp.dest('./public/stylesheets/') );
+});
+
+gulp.task('scripts', function(){
+    gulp.src([
+            './public/bower/jquery.js',
+            './public/bower/bootstrap.js',
+            './public/bower/jquery.event.swipe.js',
+            './resources/js/main.js',
+        ])
+        .pipe( concat('app.js') )
+        .pipe( gulp.dest('./public/javascript/') );
+});
+
+gulp.task('default', [], function(){
+    gulp.start('bower', 'sass', 'scripts');
+});
--- Problem3/index.php
+++ Problem3/index.php
@@ -6,26 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Problem 3</title>
 
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
-    <script src="bower_components/jquery.event.move/js/jquery.event.move.js"></script>
-    <script src="bower_components/jquery.event.swipe/js/jquery.event.swipe.js"></script>
-    <script src="assets/js/main.js"></script>
+    <link rel="stylesheet" href="public/stylesheets/app.css">
+    <script src="public/javascript/app.js"></script>
 </head>
 <body>
+    <div role="tabpanel">
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs" role="tablist" id="tablist"></ul>
 
-<div role="tabpanel">
-    <!-- Nav tabs -->
-    <ul class="nav nav-tabs" role="tablist" id="tablist"></ul>
+        <!-- Tab panes -->
+        <div class="tab-content" id="tablistcontent"></div>
 
-    <!-- Tab panes -->
-    <div class="tab-content" id="tablistcontent"></div>
-
-</div>
-<script type="text/javascript">
-$(document).ready(init);
-</script>
+    </div>
+    <script type="text/javascript">
+        $(document).ready(init);
+    </script>
 </body>
 </html>
